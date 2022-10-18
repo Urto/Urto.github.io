fa03441859fa08249c1ea1d95b7b38c1a80c9417
Urto <115885548+Urto@users.noreply.github.com>
2022-10-18 16:53:08 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1666093386,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/webvers/sprite-sheet0.png",
+		"images/webvers/shop-sheet0.png",
+		"images/webvers/moneygive-sheet0.png",
+		"images/webvers/top-sheet0.png",
+		"images/webvers/sprite2-sheet0.png",
+		"images/webvers/sprite3-sheet0.png",
+		"images/webvers/sprite4-sheet0.png",
+		"images/webvers/buyclick-sheet0.png",
+		"images/webvers/robotckicks-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
